38a81fb32202a83b8e5bbfc0717deb9841d674e3
Danniela Renderos <Danniela Renderos@DESKTOP-3TLNIMB>
2018-06-17 17:57:46 -0600

    reloj

--- src/reloj/Reloj.java
+++ src/reloj/Reloj.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package reloj;
 
 import java.util.*;
@@ -11,7 +7,7 @@ import java.util.logging.Logger;
 
 /**
  *
- * @author Danniela Renderos
+ * @author Jeniffer Merino
  */
 public class Reloj extends javax.swing.JFrame implements Runnable {
 
